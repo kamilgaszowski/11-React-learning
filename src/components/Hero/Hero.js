302da57212bbe503793808f2bca1149d2087ad6b
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-06 00:29:22 +0200

    Add 'List', 'Hero' and 'Column' components

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Hero.scss';
+import PropTypes from 'prop-types';
+
+const Hero = props => (
+  
+      <header className = {styles.component}>
+        <h2 className={styles.title}>{props.title}</h2>
+        <img className={styles.image} src={props.image} />
+      </header>
+    
+  );
+
+  Hero.propTypes = {
+title: PropTypes.node.isRequired,
+image: PropTypes.string,
+};
+
+
+export default Hero;
