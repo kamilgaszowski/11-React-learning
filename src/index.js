1bf585ad483ff753c57c9f31430f298cea395b3a
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-09 23:35:05 +0200

    Add ESLint and hukkjk

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const test = 1
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -6,6 +6,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 
-const test = 2
+
 ReactDOM.render(<App />, document.getElementById('app'));
 
