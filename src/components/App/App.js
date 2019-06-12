7177890ab6e202286987a279249c547dae9ed132
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-13 00:28:39 +0200

    Add 'Search' component - doesn't work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
+import Search from '../Search/Search.js';
 
 class App extends React.Component {
 
@@ -37,6 +38,7 @@ class App extends React.Component {
         <main className={styles.component}>
           <h1 className={styles.title}>{title}</h1>
           <h2 className={styles.subtitle}>{subtitle}</h2>
+          <Search />
           {lists.map(listData => (
             <List key={listData.id} {...listData} />
           ))}
