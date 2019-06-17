9db70efc6b531365248a059865a913c6db219d98
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-18 00:47:58 +0200

    Change 'Search' component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
-import Search from '../Search/Search.js';
+import Search from '../Search/SearchContainer';
+//import { settings } from '../../data/dataStore';
 
 class App extends React.Component {
 
@@ -15,6 +16,9 @@ class App extends React.Component {
       title: PropTypes.node,
       subtitle: PropTypes.node,
       lists: PropTypes.array,
+      searchString: PropTypes.string,
+      changeSearchString: PropTypes.func,
+  
     }
     
     addList(title){
@@ -33,12 +37,12 @@ class App extends React.Component {
       ));
     }
     render() {
-      const {title, subtitle, lists} = this.props;
+      const {title, subtitle, lists, changeSearchString} = this.props;
       return (
         <main className={styles.component}>
           <h1 className={styles.title}>{title}</h1>
           <h2 className={styles.subtitle}>{subtitle}</h2>
-          <Search />
+          <Search action={changeSearchString}/>
           {lists.map(listData => (
             <List key={listData.id} {...listData} />
           ))}
