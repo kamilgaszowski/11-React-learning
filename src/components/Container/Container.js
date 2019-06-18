d5df5d74e6ecdecaef698cefd45a67dc1bd5343b
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-18 13:35:28 +0200

    Fix component 'Container'

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,18 +2,15 @@ import React from 'react';
 import styles from './Container.scss';
 import PropTypes from 'prop-types';
 
-class Container extends React.Component {
-    static propTypes = {
-      children: PropTypes.node,
-    }
-    render (){
-        
-      return (
-        <div className={styles.component}>
-          {this.props.children}
-        </div>
-      );
-    }
-}
+const Container = props => (
+    
+  <div className={styles.component}>
+    {props.children}
+  </div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Container;
