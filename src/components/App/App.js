81fe29af507c79be57f0fcd92c522d0c960a5362
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-06-18 13:25:45 +0200

    Change component 'App'. Add components 'Home' & 'Info'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,55 +1,15 @@
 import React from 'react';
-import styles from './App.scss';
-import List from '../List/ListContainer.js';
-import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer';
-//import { settings } from '../../data/dataStore';
+import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-class App extends React.Component {
-
-    state = {
-      list: this.props.list || [],
-      
-    }
-    static propTypes = {
-      list: PropTypes.node,
-      title: PropTypes.node,
-      subtitle: PropTypes.node,
-      lists: PropTypes.array,
-      searchString: PropTypes.string,
-      changeSearchString: PropTypes.func,
-  
-    }
-    
-    addList(title){
-      this.setState(state => (
-        {
-          list: [
-            ...state.list,
-            {
-              key: state.list.length,
-              title,
-              icon: 'list-alt',
-              cards: [],
-            },
-          ],
-        }
-      ));
-    }
-    render() {
-      const {title, subtitle, lists, changeSearchString} = this.props;
-      return (
-        <main className={styles.component}>
-          <h1 className={styles.title}>{title}</h1>
-          <h2 className={styles.subtitle}>{subtitle}</h2>
-          <Search action={changeSearchString}/>
-          {lists.map(listData => (
-            <List key={listData.id} {...listData} />
-          ))}
-          
-        </main>
-      );
-    }
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/info' component={Info} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './Home.scss';
+import List from '../List/ListContainer.js';
+import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
+//import { settings } from '../../data/dataStore';
+
+class Home extends React.Component {
+
+    state = {
+      list: this.props.list || [],
+      
+    }
+    static propTypes = {
+      list: PropTypes.node,
+      title: PropTypes.node,
+      subtitle: PropTypes.node,
+      lists: PropTypes.array,
+      searchString: PropTypes.string,
+      changeSearchString: PropTypes.func,
+  
+    }
+    
+    addList(title){
+      this.setState(state => (
+        {
+          list: [
+            ...state.list,
+            {
+              key: state.list.length,
+              title,
+              icon: 'list-alt',
+              cards: [],
+            },
+          ],
+        }
+      ));
+    }
+    render() {
+      const {title, subtitle, lists, changeSearchString} = this.props;
+      return (
+        <main className={styles.component}>
+          <h1 className={styles.title}>{title}</h1>
+          <h2 className={styles.subtitle}>{subtitle}</h2>
+          <Search action={changeSearchString}/>
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
+          ))}
+          
+        </main>
+      );
+    }
+}
+
+export default Home;
